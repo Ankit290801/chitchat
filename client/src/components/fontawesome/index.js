4d20f896b2edb862d9b584395a125caf4589edcd
Bhaskar Sengupta <sajal1943@gmail.com>
2020-12-15 13:32:07 +0530

    merged

--- client/src/components/fontawesome/index.js
+++ client/src/components/fontawesome/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCoffee, faHome , faUser,faBookmark ,faBell,faSearch} from '@fortawesome/free-solid-svg-icons'
+import { faCoffee, faHome , faUser,faBookmark ,faBell,faSearch ,faGlobe} from '@fortawesome/free-solid-svg-icons'
 
 
-library.add(faCoffee,faHome,faUser,faBookmark ,faBell,faSearch);
+library.add(faCoffee,faHome,faUser,faBookmark ,faBell,faSearch ,faGlobe);
